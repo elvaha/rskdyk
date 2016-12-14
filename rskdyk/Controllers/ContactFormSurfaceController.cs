61d45dc7c4bc26fbd56b92fd37c97a0e15bac433
elvaha <elias.v.hansen@gmail.com>
2016-12-14 23:36:16 +0100

    New things added

---
+++ rskdyk/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,34 @@
+﻿using rskdyk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+
+namespace rskdyk.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactFormModelView());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactFormModelView model)
+        {
+            IContent contactForm = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "ContactForm");
+
+            contactForm.SetValue("asker", model.Name);
+            contactForm.SetValue("email", model.Email);
+            contactForm.SetValue("subject", model.Subject);
+            contactForm.SetValue("message", model.Messsage);
+
+            Services.ContentService.SaveAndPublishWithStatus(contactForm);
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ rskdyk/Models/ContactFormModelView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace rskdyk.Models
+{
+    public class ContactFormModelView
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Messsage { get; set; }
+    }
+}
